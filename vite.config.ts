2c075056fc256e4520437a734e22f1662e6d0ce3
Yulia Lisichkina <lisik_ul@mail.ru>
2026-09-12 13:52:22 +0300

    подключение tailwindcss и css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
         replaceAttrValues: { '#1F2937': 'currentColor' },
       },
     }),
+    tailwindcss()
   ],
   server: {
     proxy: {
