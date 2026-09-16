71460394f3ac775297cb7a79a0f54989ae259531
Yulia Lisichkina <lisik_ul@mail.ru>
2026-09-16 22:55:59 +0300

    папку Icons удалена

--- src/components/ui/Icons/index.tsx
+++
@@ -1,6 +0,0 @@
-// Импортируем SVG-файлы как React-компоненты с помощью специального флага ?react
-import SearchIcon from './SearchIcon.svg?react';
-import ProfileIcon from './ProfileIcon.svg?react';
-
-// Экспортируем их наружу, чтобы весь проект мог использовать их через один короткий импорт
-export { SearchIcon, ProfileIcon };
