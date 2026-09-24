cc044f601517accf4534c40d0bd3318d4a9ebcd4
Yulia Lisichkina <lisik_ul@mail.ru>
2026-09-25 01:42:14 +0300

    MainLayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../Header/Header';
-import { BottomNav } from '../BottomNav/BottomNav';
-export function MainLayout() {
+import { Header } from './Header';
+import { BottomNav } from './BottomNav/BottomNav'
+import { Breadcrumbs } from '../../components/ui'
+
+export const MainLayout = () => {
   return (
-    <div className="app-layout flex flex-col min-h-screen w-full">
+    <div className="main-layout">
       <Header />
-      <main className="app-layout__main grow py-6 max-md:py-3 max-md:pb-19">
-        <div className="app-layout__container w-full max-w-container mx-auto px-4">
+      <div className="main-layout__container">
+        <main className="main-layout__content">
+          <Breadcrumbs />
           <Outlet />
-        </div>
-      </main>
+        </main>
+      </div>
       <BottomNav />
     </div>
   );
-}
+};
